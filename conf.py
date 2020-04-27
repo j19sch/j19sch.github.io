161668cbd47f42717e874f9133b2392433fc4187
Joep Schuurkes <j19sch@gmail.com>
2020-04-27 21:05:02 +0200

    adds learning clojure project and first blog post

--- conf.py
+++ conf.py
@@ -278,6 +278,7 @@ TIMEZONE = "Europe/Amsterdam"
 # Used by babel.dates, CLDR style: http://cldr.unicode.org/translation/date-time
 # You can also use 'full', 'long', 'medium', or 'short'
 # DATE_FORMAT = 'YYYY-MM-dd HH:mm'
+DATE_FORMAT = 'd MMMM yyyy'
 
 # Date format used to display post dates, if local dates are used. (translatable)
 # Used by moment.js: https://momentjs.com/docs/#/displaying/format/
