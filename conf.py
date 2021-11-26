a4028e15ff64b7248c2d07768b359f10e076f70f
jss <j19sch@gmail.com>
2021-11-26 20:08:50 +0100

    adds plausible analytics

--- conf.py
+++ conf.py
@@ -1254,6 +1254,7 @@ GENERATE_ATOM = True
 EXTRA_HEAD_DATA = """
 <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101252313);</script>
 <script async src="//static.getclicky.com/js"></script>
+<script defer data-domain="j19sch.github.io" src="https://plausible.io/js/plausible.js"></script>
 """
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
